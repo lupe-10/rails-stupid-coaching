7853ebc333dceb3cdab22718a17dfe2b107b842b
Lucia diez <lupe.diez@gmail.com>
2021-02-15 15:10:55 -0300

    app funcionando, sin estilo

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+
+  def answer
+    @question = params[:question]
+    @answer = 
+      if @question.end_with?('?')
+        'Silly question, get dressed and go to work!.'
+      elsif @question == 'I am going to work!'
+        'Great!'
+      else
+        "I don't care, get dressed and go to work!"
+      end
+  end
+
+  def ask
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'quetions#ask'
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'  
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
